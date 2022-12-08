a65b0812df98e3ad64ef27f17402cd5b7dc7de60
CkMadhavan <madhavan.champion@gmail.com>
2022-12-08 11:32:43 +0000

    Added Discount Not Eligible Test

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -115,5 +115,36 @@ namespace Supermarket.UnitTests
             Assert.AreEqual(total, 364);  // (1*130) (A99) + (1*45 + 1*30) (B15) + 1*60 (C40) + 1*99 (T34) = 364
         }
 
+        [TestMethod]
+        public void totalPrice_DiscountNotEligible_508()
+        {
+            // Arrange
+            List<string> skus = new List<string>();
+            Dictionary<string, int> prices = new Dictionary<string, int>();
+            Dictionary<string, Tuple<int, int>> discounts = new Dictionary<string, Tuple<int, int>>();
+            List<string> skus_to_buy = new List<string>();
+
+            skus = new List<string>() { "A99", "B15", "C40", "T34" };
+
+            prices.Add(skus[0], 50);
+            prices.Add(skus[1], 30);
+            prices.Add(skus[2], 60);
+            prices.Add(skus[3], 99);
+
+            discounts.Add(skus[0], Tuple.Create(3, 130));
+            discounts.Add(skus[1], Tuple.Create(2, 45));
+            discounts.Add(skus[2], Tuple.Create(0, 0));
+            discounts.Add(skus[3], Tuple.Create(0, 0));
+
+            skus_to_buy = new List<string>() { "A99", "B15","T34",  "A99", "T34", "C40", "C40", "C40" };
+
+            // Act
+            int total = SupermarketCheckout.totalPrice(skus, prices, discounts, skus_to_buy);
+            Console.WriteLine(total);
+
+            // Assert
+            Assert.AreEqual(total, 508);  // 2*50 (A99) + 1*30 (B15) + 3*60 (C40) + 2*99 (T34) = 508
+        }
+
     }
 }
